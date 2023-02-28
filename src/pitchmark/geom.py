5588d06e4b43bd2dff5b2e9aa0056b4267de8e1a
Istvan Kleijn <istvankleijn@users.noreply.github.com>
2023-02-28 18:40:02 +0000

    feat: Clip and filter LAS/LAZ files

--- src/pitchmark/geom.py
+++ src/pitchmark/geom.py
@@ -1,3 +1,4 @@
+import pyproj
 import shapely
 
 
@@ -10,3 +11,19 @@ def polygon_from_geojson(geom_string):
         )
     polygon = geoms[0]
     return polygon
+
+
+def prepared_shell(geoseries, *, distance=0.0, crs_to=None):
+    """
+    Construct a shell (buffer) around the (unary union of the) geometries in a given
+    Geoseries, optionally transform it to a different CRS, and prepare it to improve
+    computational efficiency of subsequent operations.
+    """
+    masked_area = geoseries.unary_union.buffer(distance)
+
+    if crs_to is not None:
+        transformer = pyproj.Transformer.from_crs(geoseries.crs, crs_to, always_xy=True)
+        masked_area = shapely.ops.transform(transformer.transform, masked_area)
+
+    shapely.prepare(masked_area)
+    return masked_area
